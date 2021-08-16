49194f3727004569ab977d53a99156b70b394437
pnowakowski614 <p.nowakowski416@gmail.com>
2021-08-16 18:51:56 +0200

    Layout fixes, added few more functionalities

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.getElementById("container");
 const clear = document.getElementById("clear");
-let divs = document.getElementsByClassName("grid-div");
+const eraser = document.getElementById("eraser");
+const black = document.getElementById("black");
+const random = document.getElementById("random");
+const gridlines = document.getElementById("gridlines");
+const color = document.getElementById("color");
+const input = document.createElement("input");
 const newGrid = document.getElementById("newgrid");
+let divs = document.getElementsByClassName("grid-div");
 let currentGridSize = 16;
+input.type = "color";
+input.id = "input";
 MakingGrids(currentGridSize);
 
 function MakingGrids(currentGridSize) {
@@ -17,8 +25,39 @@ function MakingGrids(currentGridSize) {
     }
 }
 
-container.addEventListener('mouseover', function(e) {
-    e.target.style.backgroundColor = "black";
+eraser.addEventListener('click', function () {
+    container.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = "white";
+    })
+})
+
+black.addEventListener('click', function () {
+    container.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = "black";
+    })
+})
+
+gridlines.addEventListener('click', function () {
+    for (let i = 0; i < currentGridSize*currentGridSize; ++i) {
+        if (divs[i].style.border === "0px none") {
+            divs[i].style.border = "1px rgb(190, 190, 190) solid";
+        }
+        else {
+            divs[i].style.border = "0px";
+        }
+    }
+})
+
+color.addEventListener('change', function () {
+    container.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = color.value;
+    })
+});
+
+random.addEventListener('click', function () {
+    container.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+    })
 })
 
 clear.addEventListener('click', function() {
